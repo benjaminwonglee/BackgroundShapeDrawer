c1d5ce84fcb442f06bcb6ab24d068944ae28979e
benjaminwonglee <benjaminwonglee@gmail.com>
2017-05-27 14:53:13 +1200

    Changed colors to be more appropriate. Will add more later

--- src/main/ColorChooser.java
+++ src/main/ColorChooser.java
@@ -11,31 +11,40 @@ public class ColorChooser {
 		color = null;
 		switch (colorInt) {
 		case (1):
+			// General blue
 			color = new Color(20, 20, 200);
 			break;
 		case (2):
-			color = new Color(200, 200, 0);
+			// Sea blue
+			color = new Color(122, 100, 154);
 			break;
 		case (3):
-			color = new Color(20, 200, 0);
-			break;
-		case (4):
+			// Light Blue
 			color = new Color(100, 200, 200);
 			break;
-		case (5):
-			color = new Color(200, 100, 100);
-			break;
-		case (6):
+		case (4):
+			// Light Purple (Lilac)
 			color = new Color(200, 100, 200);
 			break;
+		case (5):
+			// General Yellow
+			color = new Color(200, 200, 0);
+			break;
+		case (6):
+			// Gold
+			color = new Color(178, 204, 51);
+			break;
 		case (7):
-			color = new Color(145, 100, 155);
+			// General Green
+			color = new Color(20, 200, 0);
 			break;
 		case (8):
-			color = new Color(145, 155, 100);
+			// Dark red
+			color = new Color(120, 0, 0);
 			break;
 		case (9):
-			color = new Color(120, 0, 0);
+			// Violet (Red)
+			color = new Color(200, 100, 100);
 			break;
 		default:
 			color = new Color(200, 0, 0);
